dfb2984f848be10b2ca64cfcf847302666728f73
Jiuru-Kun <1360223184@qq.com>
2025-07-25 22:17:13 +0800

    Update changeDisplayStyle.js

--- javascript/changeDisplayStyle.js
+++ javascript/changeDisplayStyle.js
@@ -9,14 +9,26 @@ function changeDisplayStyle(){
 
   if (localStorage.getItem("style") == 0){
     button.innerHTML = "日间模式";
-    localStorage.setItem("style", 1)
+    localStorage.setItem("style", 1);
 
-    root.style.setProperty('--bgColor', rgba(20, 20, 20, 1));
-    root.style.setProperty('--headTextColor', 'black');
-    root.style.setProperty('--textColor', 'white');
+
+    root.style.setProperty('--bgColor', 'rgba(20, 20, 20, 1)');
+    root.style.setProperty('--boxBgColor', 'rgba(140, 140, 140, 0.12)');
+    root.style.setProperty('--textColor', 'rgba(200, 200, 200, 1)');
+    root.style.setProperty('--headBoxColor', 'rgba(0, 100, 111, 1)');
+    root.style.setProperty('--headTextColor', 'rgba(237, 242, 242, 1)');
+    root.style.setProperty('--hrColor', '#808080');
+
   }
   else if (localStorage.getItem("style") == 1) {
     button.innerHTML = "夜间模式";
     localStorage.setItem("style", 0)
+
+    root.style.setProperty('--bgColor', 'rgba(237, 242, 242, 1)');
+    root.style.setProperty('--boxBgColor', 'rgba(170, 170, 170, 0.12)');
+    root.style.setProperty('--textColor', 'black');
+    root.style.setProperty('--headBoxColor', '#009ca7');
+    root.style.setProperty('--headTextColor', 'white');
+    root.style.setProperty('--hrColor', '#b3b3b3');
   }
 }
