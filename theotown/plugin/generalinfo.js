21fafe2e9be8c3a981fbf042520c1a5ed28283de
Jiuru-Kun <1360223184@qq.com>
2025-07-30 00:25:08 +0800

    Create generalinfo.js

---
+++ theotown/plugin/generalinfo.js
@@ -0,0 +1,9 @@
+const version = "1.1.1";
+const title = "test";
+const news = "Version 1.1.1: test";
+
+var versionInfo = document.getElementById("versionInfo");
+var newsTitle = document.getElementById("newsTitle");
+var newsInfo = document.getElementById("newsInfo");
+
+versionInfo.innerText = version;
