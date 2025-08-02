484b6aecb371fdcd06894eaffbc296d97bfbc69d
Jiuru-Kun <1360223184@qq.com>
2025-08-02 20:07:25 +0800

    Update generalinfo.js

--- theotown/plugin/generalinfo.js
+++ theotown/plugin/generalinfo.js
@@ -1,9 +1,16 @@
-const version = "1.1.1";
-const title = "test";
-const news = "Version 1.1.1: test";
+// 内容
+const version = "1.1.2";
+const title = "";
+const news = "Version 1.1.2: 进行了一些修改";
 
 var versionInfo = document.getElementById("versionInfo");
 var newsTitle = document.getElementById("newsTitle");
 var newsInfo = document.getElementById("newsInfo");
 
 versionInfo.innerText = version;
+if (newsTitle != null) {
+    newsTitle.innerText = title;
+}
+if (newsInfo != null) {
+    newsInfo.innerText = news;
+}
