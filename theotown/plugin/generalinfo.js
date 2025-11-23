4264659650c22d9a528ccee9337a9c83715f4f25
Jiuru-Kun <1360223184@qq.com>
2025-11-23 10:48:12 +0800

    Update generalinfo.js

--- theotown/plugin/generalinfo.js
+++ theotown/plugin/generalinfo.js
@@ -1,7 +1,7 @@
 // 内容
-const version = "1.2.1";
+const version = "1.2.2";
 const title = "";
-const news = "<em>Version 1.2.0:</em> <br>1.做了一个索引页<br>2.增加了分类的JSON生成器<br>3.增加了基础建筑那里的分类选项<br>下次再见～<br><em>Version 1.2.1:</em> 修复了一个错误";
+const news = "<em>Version 1.2.0:</em> <br>1.做了一个索引页<br>2.增加了分类的JSON生成器<br>3.增加了基础建筑那里的分类选项<br>下次再见～<br><em>Version 1.2.1:</em> 修复了一个错误<br><em>Version 1.2.2:</em> <br>做了一个数值计算器";
 
 var versionInfo = document.getElementById("versionInfo");
 var newsTitle = document.getElementById("newsTitle");
