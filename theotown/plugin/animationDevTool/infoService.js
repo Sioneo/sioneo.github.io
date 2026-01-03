3bd58b612b2754ee25e1cbdfb3b0151ec6738b5d
Jiuru <1360223184@qq.com>
2026-01-03 16:15:39 +0800

    fix

--- theotown/plugin/animationDevTool/infoService.js
+++ theotown/plugin/animationDevTool/infoService.js
@@ -12,7 +12,7 @@ function updateInfo() {
         // Load animation list information
         let animationListInfo = "";
         for (let j = 0; j < animationData.length; j++) {
-            let result = `Animation(${j}): ${animationData[j].name}, y: ${animationData[j].x}, y: ${animationData[j].y}, handle x: ${-animationData[j].x}, handle y: ${-animationData[j].y}<br>`
+            let result = `Animation(${j}): ${animationData[j].name}, x: ${animationData[j].x}, y: ${animationData[j].y}, handle x: ${-animationData[j].x}, handle y: ${-animationData[j].y}<br>`
             animationListInfo += result;
         }
         document.getElementById("animationList").innerHTML = animationListInfo;
