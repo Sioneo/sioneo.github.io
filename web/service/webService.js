35e31e9da8d52c83208d9eca12bd98df64474a06
Sioneo <1360223184@qq.com>
2026-01-18 14:52:35 +0800

    update

--- web/service/webService.js
+++ web/service/webService.js
@@ -1,3 +1,28 @@
+// 判断是否在移动端
+function getDeviceType() {
+    const userAgent = navigator.userAgent;
+    const isAndroid = /Android/i.test(userAgent);
+    const isIOS = /iPhone|iPad|iPod/i.test(userAgent);
+    const isWindowsPhone = /Windows Phone/i.test(userAgent);
+
+    if (isAndroid || isIOS || isWindowsPhone) {
+        if (/(tablet|ipad|playbook|silk)|(android(?!.*mobile))/i.test(userAgent)) {
+            return 'tablet';
+        }
+        return 'mobile';
+    }
+
+    return 'desktop';
+}
+
+if (getDeviceType() == "desktop") {
+
+} else if (getDeviceType == "tablet") {
+    document.documentElement.style.setProperty("--style-side-padding", "50px");
+} else {
+    document.documentElement.style.setProperty("--style-side-padding", "10px");
+}
+
 // Update time
 const Web = {
     showPageUpdateTime: function () {
