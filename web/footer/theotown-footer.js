cd03347321498c66ba6c250b32041ebf62e0b9fe
Sioneo <1360223184@qq.com>
2026-02-11 22:13:51 +0800

    update

--- web/footer/theotown-footer.js
+++ web/footer/theotown-footer.js
@@ -2,10 +2,12 @@ class TheotownFooter extends HTMLElement {
     connectedCallback() {
         this.innerHTML = `
         <footer class="theotown-footer">
-            <p>最后一次更新：<span id="webInfoUpdateTime"></span></p>
             <div class="footer-box">
-                <p>本站基于<a href="https://githubdocs.cn/en/pages/getting-started-with-github-pages/about-github-pages"
-                        target="_blank" class="github-page-note"><i class="w">Q</i>GitHub Pages</a>搭建</p>
+                <div class="footer-about">
+                    <h6>AzureWiki</h6>
+                    <a href="https://forum.theotown.com/" target="_blank">论坛</a>
+                    <a href="https://pd.qq.com/s/89j7ypse8" target="_blank">QQ交流频道</a>
+                </div>
                 <div class="footer-about">
                     <a href="https://www.azureneko.top/theotown/onionpedia.html" target="_blank">关于本站</a>
                     <a href="https://www.azureneko.top/theotown/me.html" target="_blank">关于我</a>
@@ -13,16 +15,6 @@ class TheotownFooter extends HTMLElement {
             </div>
         </footer>
         `;
-        
-        // 直接调用 Web 函数，但需要等待 DOM 更新
-        setTimeout(() => {
-            if (window.Web && Web.showPageUpdateTime) {
-                Web.showPageUpdateTime();
-            } else {
-                console.warn('Web 对象或 showPageUpdateTime 方法未找到');
-                this.updateTimeFallback();
-            }
-        }, 0);
     }
     
     updateTimeFallback() {
