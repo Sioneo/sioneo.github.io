847a852230b12b0609e7d78a186fec0299b901f9
Jiuru-Kun <1360223184@qq.com>
2026-02-26 01:03:33 +0800

    Fix processor.js
    Omg扣错字了

--- theotown/plugin/building-js/processor.js
+++ theotown/plugin/building-js/processor.js
@@ -396,8 +396,8 @@ document.getElementById("main-submit-button").addEventListener("click", function
             // 水电
             let water = document.getElementById("water-input").value;
             let power = document.getElementById("power-input").value;
-            basicData.water = Nunber.isNaN(water)? null: water;
-            basicData.power = Nunber.isNaN(power)? null: power;
+            basicData.water = Number.isNaN(water)? null: water;
+            basicData.power = Number.isNaN(power)? null: power;
             // 影响
             for (let id in influenceInputData) {
                 let influence = influenceInputData[id];
